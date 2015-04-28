468ba3182557e6c6cfb3296136c572cf4e2cbe83
prateekchandan <prateekchandan5545@gmail.com>
2015-04-29 03:36:38 +0530

    UPdated HMM

--- lab5/runner.sh
+++ lab5/runner.sh
@@ -5,10 +5,10 @@ rm -rf data/r_answer_*
 
 for i in `seq 1 5`
 do
-	python viterbi.py data/tables_$i.txt < data/test_data_$i > data/answer_$i &
+	python viterbi.py data/tables_$i.txt < data/test_data_$i > data/answer_$i 
 done
 
 for i in `seq 1 5`
 do
-	python r_viterbi.py data/r_tables_$i.txt < data/r_test_data_$i > data/r_answer_$i &
+	python r_viterbi.py data/r_tables_$i.txt < data/r_test_data_$i > data/r_answer_$i 
 done
